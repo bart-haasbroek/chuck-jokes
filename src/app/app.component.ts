533086af580c2e6a00dbb346bf742bc9b45c63d5
Bart Haasbroek <haasbroek_89@hotmail.com>
2020-11-12 12:12:57 +0100

    Add a button to fetch jokes and display them in a list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
 import * as fromChuckJokes from '@store/chuck-jokes';
 import { chuckJokesState } from '@store/chuck-jokes';
+import { Observable } from 'rxjs';
+import { ChuckJokeInterface } from '@interfaces/chuck-joke.interface';
 
 
 @Component({
@@ -9,9 +11,16 @@ import { chuckJokesState } from '@store/chuck-jokes';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  public chuckJokes$: Observable<ChuckJokeInterface[]>;
   constructor(private store: Store<chuckJokesState>) {
+  }
+
+  ngOnInit(): void {
+    this.chuckJokes$ = this.store.select(fromChuckJokes.selectAllChuckJokes);
+  }
+
+  public getJokes(): void {
     this.store.dispatch(fromChuckJokes.getChuckJokes());
-    this.store.select(fromChuckJokes.selectAllChuckJokes).subscribe(console.log);
   }
 }
